4ef95f92968fcd9770dc7692f519bbf85e8abc97
eecheverry <eecheverry@nubark.com>
2019-11-12 16:14:24 -0500

    Refactor sql_migrate function default parameters

--- migrark/api.py
+++ migrark/api.py
@@ -4,9 +4,10 @@ from .versioner import SqlVersioner
 from .migrator import Migrator
 
 
-def sql_migrate(database_uri, migrations_path, schema='__template__',
-                context={}, target_version='999999'):
-
+def sql_migrate(database_uri, migrations_path, schema=None,
+                context={}, target_version=None):
+    schema = schema or '__template__'
+    target_version = target_version or '999999'
     with connect(database_uri) as connection:
         migration_context = {
             'migrations_path': migrations_path,
